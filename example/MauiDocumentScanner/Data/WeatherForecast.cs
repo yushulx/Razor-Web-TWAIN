55b42c6595a3e11625d39f943e2a89e72ff08c3e
yushulx <lingxiao1002@gmail.com>
2023-04-25 14:17:18 +0800

    Added .NET MAUI Blazor example

---
+++ example/MauiDocumentScanner/Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace MauiDocumentScanner.Data;
+
+public class WeatherForecast
+{
+	public DateTime Date { get; set; }
+
+	public int TemperatureC { get; set; }
+
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+	public string Summary { get; set; }
+}
---
+++ example/MauiDocumentScanner/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDocumentScanner;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
